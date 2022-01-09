4c52e509a5ad4991fd364947089cd47189431241
Rafael Tavares <rafatavares03@hotmail.com>
2022-01-09 19:04:12 -0300

    conclude tablet header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,27 +7,28 @@ function Header() {
   return (
     <header className="header py-4 py-md-0">
       <div className="align-items-center d-flex h-100 justify-content-between">
-        <img src={logo} alt="Logo" className="logo ms-4"/>
+        <img src={logo} alt="Logo" className="logo"/>
+        <div className="header-line"></div>
         <nav className="d-none d-md-block navbar header-nav p-0">
-          <ul className="d-flex list-unstyled m-0 nav-container p-lg-0">
+          <ul className="d-flex list-unstyled m-0 nav-container p-0">
             <li className="nav-item text-center">
-              <NavLink to="/" className="header-link mx-lg-4 nav-link p-0 text-uppercase text-white">
-              <em className="d-none d-lg-inline-block fst-normal fw-bold">00</em> Home
+              <NavLink to="/" className="header-link link mx-lg-4 nav-link p-0 text-uppercase text-white">
+              <em className="d-none d-lg-inline-block fst-normal fw-bold">00</em>Home
               </NavLink>
             </li>
             <li className="nav-item text-center">
-              <NavLink to="/destination" className="header-link mx-lg-4 nav-link p-0 text-uppercase text-white">
-              <em className="d-none d-lg-inline-block fst-normal fw-bold">01</em> Destination
+              <NavLink to="/destination" className="header-link link mx-lg-4 nav-link p-0 text-uppercase text-white">
+              <em className="d-none d-lg-inline-block fst-normal fw-bold">01</em>Destination
               </NavLink>
             </li>
             <li className="nav-item text-center">
-              <NavLink to="/crew" className="header-link mx-lg-4 nav-link p-0 text-uppercase text-white">
-              <em className="d-none d-lg-inline-block fst-normal fw-bold">02</em> Crew
+              <NavLink to="/crew" className="header-link link mx-lg-4 nav-link p-0 text-uppercase text-white">
+              <em className="d-none d-lg-inline-block fst-normal fw-bold">02</em>Crew
               </NavLink>
             </li>
             <li className="nav-item text-center">
-              <NavLink to="/technology" className="header-link mx-lg-4 nav-link p-0 text-uppercase text-white">
-              <em className="d-none d-lg-inline-block fst-normal fw-bold">03</em> Technology
+              <NavLink to="/technology" className="header-link link mx-lg-4 nav-link p-0 text-uppercase text-white">
+              <em className="d-none d-lg-inline-block fst-normal fw-bold">03</em>Technology
               </NavLink>
             </li>
           </ul>
