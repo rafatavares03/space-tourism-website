fc7366f3d9ed3f5d2383bdb8df55b8b9b840e0a7
Rafael Tavares <rafatavares03@hotmail.com>
2022-01-12 23:24:05 -0300

    home text styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,11 +11,11 @@ function Home() {
         <source media="(min-width: 540px)" srcSet={bgTablet} className="h-100 w-100"/>
         <img src={bgMobile} alt="Background" className="h-100 w-100"/>
       </picture>
-      <div className="container">
+      <div className="container home">
         <div>
-          <p className="">So you want to travel to</p>
-          <h1 className="text-white">Space</h1>
-          <p className="">
+          <p className="home-description text-uppercase">So, you want to travel to</p>
+          <h1 className="home-title text-uppercase text-white">Space</h1>
+          <p className="text">
             Let's face it; if you want to go to space, you might as well
             genuinely go to outer space and note hover kind of on the 
             edge of it. Well sit back, and relax because we'll gave you a 
