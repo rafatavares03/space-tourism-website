db95790a51c6f25fa968eba9d13f336fa5955f68
Rafael Tavares <rafatavares03@hotmail.com>
2022-02-13 00:12:45 -0300

    modal items basic style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import {
   Modal,
   ModalHeader,
   ModalBody,
+  Navbar,
   NavItem
 } from 'reactstrap';
 import logo from '../assets/images/shared/logo.svg';
@@ -20,41 +21,64 @@ export default function Header() {
         <div className="align-items-center d-flex header-container h-100 justify-content-between position-relative">
           <img src={logo} alt="Logo" className="logo" />
           <div className="d-none d-xxl-block header-line"></div>
-          <nav className="d-none d-md-block header-nav navbar p-0">
+          <Navbar container={false} className="d-none d-md-block p-0">
             <ul className="d-flex list-unstyled m-0 nav-container p-0">
               <NavItem className="text-center">
-                <NavLink to="/" className="header-link link mx-lg-4 nav-link p-0 text-uppercase text-white">
+                <NavLink to="/" className="header-link mx-lg-4 nav-link navbar-items p-0 text-uppercase text-white">
                   <em className="d-none d-lg-inline-block fst-normal fw-bold">00</em>Home
                 </NavLink>
               </NavItem>
               <NavItem className="text-center">
-                <NavLink to="/destination" className="header-link link mx-lg-4 nav-link p-0 text-uppercase text-white">
+                <NavLink to="/destination" className="header-link mx-lg-4 nav-link navbar-items p-0 text-uppercase text-white">
                   <em className="d-none d-lg-inline-block fst-normal fw-bold">01</em>Destination
                 </NavLink>
               </NavItem>
               <NavItem className="text-center">
-                <NavLink to="/crew" className="header-link link mx-lg-4 nav-link p-0 text-uppercase text-white">
+                <NavLink to="/crew" className="header-link mx-lg-4 nav-link navbar-items p-0 text-uppercase text-white">
                   <em className="d-none d-lg-inline-block fst-normal fw-bold">02</em>Crew
                 </NavLink>
               </NavItem>
               <NavItem className="text-center">
-                <NavLink to="/technology" className="header-link link mx-lg-4 nav-link p-0 text-uppercase text-white">
+                <NavLink to="/technology" className="header-link mx-lg-4 nav-link navbar-items p-0 text-uppercase text-white">
                   <em className="d-none d-lg-inline-block fst-normal fw-bold">03</em>Technology
                 </NavLink>
               </NavItem>
             </ul>
-          </nav>
-          <img src={toggle} alt="Menu" className="d-md-none me-4" onClick={togglePopup}/>
+          </Navbar>
+          <img src={toggle} alt="Menu" className="d-md-none me-4" onClick={togglePopup} />
         </div>
       </header>
       <Modal isOpen={modal} toggle={togglePopup}>
-        <ModalHeader 
-        className="border-0 p-4"
-        close={<img src={close} alt="close" onClick={togglePopup}></img>}
-        toggle={togglePopup}
+        <ModalHeader
+          className="border-0 modal-header"
+          close={<img src={close} alt="close" onClick={togglePopup}></img>}
+          toggle={togglePopup}
         />
         <ModalBody>
-          
+          <Navbar container={false} className="p-0">
+            <ul className="d-flex flex-column list-unstyled m-0 nav-container p-0">
+              <NavItem>
+                <NavLink to="/" className="mx-lg-4 nav-link navbar-items p-0 text-uppercase text-white">
+                  <em className="d-inline-block fst-normal fw-bold">00</em>Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/destination" className="mx-lg-4 nav-link navbar-items p-0 text-uppercase text-white">
+                  <em className="d-inline-block fst-normal fw-bold">01</em>Destination
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/crew" className="mx-lg-4 nav-link navbar-items p-0 text-uppercase text-white">
+                  <em className="d-inline-block fst-normal fw-bold">02</em>Crew
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/technology" className="mx-lg-4 nav-link navbar-items p-0 text-uppercase text-white">
+                  <em className="d-inline-block fst-normal fw-bold">03</em>Technology
+                </NavLink>
+              </NavItem>
+            </ul>
+          </Navbar>
         </ModalBody>
       </Modal>
     </>
